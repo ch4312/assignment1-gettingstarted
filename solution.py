caa0cdc80539570a21ce6912656d8162d80017d5
ch4312 <ch4312@nyu.edu>
2022-01-28 23:33:36 -0500

    Initial commit

--- solution.py
+++ solution.py
@@ -30,6 +30,6 @@ def welcome_assignment_answers(question):
 
 if __name__ == "__main__":
     #use this space to debug and verify that the program works
-    debug_question = "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number"
+    debug_question = "Are encoding and encryption the same? - Yes/No"
     print(welcome_assignment_answers(debug_question))
 
